9b2103fb37b11e21597a7bc30579522012224b40
gennyble <gen@nyble.dev>
2026-09-06 23:04:02 -0500

    use more rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{
-    ffi::CString,
+    ffi::{c_uint, c_void},
+    fs::File,
+    io::Read,
     os::{
-        fd::AsRawFd,
+        fd::{AsRawFd, FromRawFd, IntoRawFd},
         unix::net::{UnixListener, UnixStream},
     },
     process::Command,
+    time::Duration,
 };
 
 fn main() {
@@ -24,39 +27,38 @@ fn main() {
 }
 
 const SOCKET_PATH: &'static str = "socket";
+const MSGDATA: &'static str = "msgdata";
+const MSGDATA_LEN: usize = MSGDATA.len();
+
 fn send() -> ! {
     let us = UnixListener::bind(SOCKET_PATH).unwrap();
+    println!("[SEND] Opened UnixSocket for recv");
 
     // Launch the child binary
     let mut cmd = Command::new("target/debug/january");
     cmd.arg("recv");
     let mut child = cmd.spawn().unwrap();
+    println!("[SEND] Launched child binary");
 
-    let mut fds = [0 as libc::c_int; 2];
-    let result = unsafe { libc::socketpair(libc::AF_UNIX, libc::SOCK_STREAM, 0, fds.as_mut_ptr()) };
-    if result == -1 {
-        eprintln!("Failed to spawn socketpair");
-        child.kill().unwrap();
-        std::process::exit(-1);
-    }
+    let mut file = File::open("testfile").unwrap();
+    let mut read_msg = String::from("[SEND] Read from file: ");
+    read_three_from_file(&mut file, &mut read_msg);
+    println!("{read_msg}");
 
-    unsafe {
-        libc::fcntl(fds[0], libc::F_SETFL, libc::O_NONBLOCK);
-        libc::fcntl(fds[1], libc::F_SETFL, libc::O_NONBLOCK);
-    }
+    let file_fd = file.into_raw_fd();
+    let file_fd_len = std::mem::size_of_val(&file_fd) as c_uint;
 
-    let msg_content = CString::new("ab").unwrap();
     let mut io = libc::iovec {
-        iov_base: msg_content.as_ptr() as *mut libc::c_void,
-        iov_len: msg_content.count_bytes(),
+        iov_base: MSGDATA.as_ptr() as *mut c_void,
+        iov_len: MSGDATA_LEN,
     };
-    let cmsg_len = unsafe { libc::CMSG_SPACE(std::mem::size_of_val(&fds[1]) as libc::c_uint) };
+    let cmsg_len = unsafe { libc::CMSG_SPACE(file_fd_len) };
     let mut buffer = vec![0; cmsg_len as usize];
 
     let msg = libc::msghdr {
         msg_iov: &mut io,
         msg_iovlen: 1,
-        msg_control: buffer.as_mut_ptr() as *mut libc::c_void,
+        msg_control: buffer.as_mut_ptr() as *mut c_void,
         msg_controllen: buffer.len() as u32,
 
         // Not in the example gist? What are these?
@@ -69,25 +71,26 @@ fn send() -> ! {
     unsafe {
         (*cmsg).cmsg_level = libc::SOL_SOCKET;
         (*cmsg).cmsg_type = libc::SCM_RIGHTS;
-        (*cmsg).cmsg_len = libc::CMSG_LEN(std::mem::size_of_val(&fds[1]) as libc::c_uint);
+        (*cmsg).cmsg_len = libc::CMSG_LEN(file_fd_len);
         let cmsg_data = libc::CMSG_DATA(cmsg) as *mut i32;
-        *cmsg_data = fds[1];
+        *cmsg_data = file_fd;
     }
 
     let (client, _) = us.accept().unwrap();
     let client_fd = client.as_raw_fd();
 
+    println!("[SEND] Attempting to sendmsg");
     unsafe {
         let sent = libc::sendmsg(client_fd, &msg, 0);
-        if sent != 2 {
-            eprintln!("Expected to send 2 bytes, actually sent {sent}");
+        if sent as usize != MSGDATA_LEN {
+            eprintln!("Expected to send {MSGDATA_LEN} bytes, actually sent {sent}");
             child.kill().unwrap();
             std::process::exit(-1);
         }
-
-        let write_content = CString::new("yz").unwrap();
-        libc::write(fds[0], write_content.as_ptr() as *mut libc::c_void, 2);
     }
+
+    println!("[SEND] Sleeping for 2 seconds for recv to get message");
+    std::thread::sleep(Duration::from_secs(2));
 
     std::process::exit(0);
 }
@@ -95,20 +98,22 @@ fn send() -> ! {
 fn recv() -> ! {
     let us = UnixStream::connect(SOCKET_PATH).unwrap();
     let us_fd = us.as_raw_fd();
+    let us_fd_len = std::mem::size_of_val(&us_fd) as c_uint;
+    println!("[RECV] Opened UnixStream to send");
 
-    let mut io_buffer = [0u8 as libc::c_char; 2];
+    let mut io_buffer = [0u8 as libc::c_char; MSGDATA_LEN];
     let mut io = libc::iovec {
-        iov_base: io_buffer.as_mut_ptr() as *mut libc::c_void,
-        iov_len: 2,
+        iov_base: io_buffer.as_mut_ptr() as *mut c_void,
+        iov_len: MSGDATA_LEN,
     };
 
     // Using `us_fd` here just as a stand in for the type of a file descriptor
-    let cmsg_len = unsafe { libc::CMSG_SPACE(std::mem::size_of_val(&us_fd) as libc::c_uint) };
+    let cmsg_len = unsafe { libc::CMSG_SPACE(us_fd_len) };
     let mut cmsg_buffer = vec![0; cmsg_len as usize];
     let mut msg = libc::msghdr {
         msg_iov: &mut io,
         msg_iovlen: 1,
-        msg_control: cmsg_buffer.as_mut_ptr() as *mut libc::c_void,
+        msg_control: cmsg_buffer.as_mut_ptr() as *mut c_void,
         msg_controllen: cmsg_buffer.len() as u32,
 
         // Not in the example gist? What are these?
@@ -117,15 +122,16 @@ fn recv() -> ! {
         msg_flags: 0,
     };
 
+    println!("[RECV] Attempting to recvmsg");
     unsafe {
         let recv = libc::recvmsg(us_fd, &mut msg, libc::MSG_WAITALL);
-        if recv != 2 {
-            eprintln!("Expected to recv 2 bytes, got {recv} bytes");
+        if recv as usize != MSGDATA_LEN {
+            eprintln!("Expected to recv {MSGDATA_LEN} bytes, got {recv} bytes");
             std::process::exit(-1);
         }
     }
 
-    print!("Message received over Unix Socket: ");
+    print!("[RECV] Message received over Unix Socket: ");
     for by in io_buffer {
         print!("{}", by as u8 as char);
     }
@@ -134,23 +140,22 @@ fn recv() -> ! {
     let cmsg = unsafe { libc::CMSG_FIRSTHDR(&mut msg) };
     let cmsg_data = unsafe { libc::CMSG_DATA(cmsg) as *mut i32 };
     let recv_fd = unsafe { *cmsg_data };
-    println!("Received file descriptor: {recv_fd}");
+    println!("[RECV] Received file descriptor: {recv_fd}");
 
-    let mut recv_buffer = [0u8 as libc::c_char; 2];
-    unsafe {
-        let recv = libc::read(recv_fd, recv_buffer.as_mut_ptr() as *mut libc::c_void, 2);
-        if recv != 2 {
-            eprintln!("[SENTFD] Expected to recv 2 bytes, got {recv} bytes");
-            std::process::exit(-1);
-        }
-    }
-
-    print!("Message received over Sent Descriptor: ");
-    for by in recv_buffer {
-        print!("{}", by as u8 as char);
-    }
-    println!();
+    let mut file = unsafe { File::from_raw_fd(recv_fd) };
+    let mut read_msg = String::from("[RECV] Read from file: ");
+    read_three_from_file(&mut file, &mut read_msg);
+    println!("{read_msg}");
 
     println!("recv proccess exiting!");
     std::process::exit(0);
+}
+
+fn read_three_from_file(file: &mut File, buffer: &mut String) {
+    let mut buf = [0u8; 3];
+    file.read_exact(&mut buf).unwrap();
+
+    for by in buf {
+        buffer.push(by as char);
+    }
 }
